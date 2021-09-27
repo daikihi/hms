02decbfe07f48d35ee7a7c7c75e9d2b36cd8aabc
daiki <daiki@local>
2021-09-28 08:02:41 +0900

    add a comment line

--- src/controllers/apis/beekeepers.rs
+++ src/controllers/apis/beekeepers.rs
@@ -3,7 +3,10 @@ use crate::models::beekeeper::Beekeeper;
 use crate::use_cases::apis::beekeepers::get_all_beekeepers_use_case;
 use serde::{Serialize, Deserialize};
 
-// bk_index is used for api
+/**
+ * bk_index is used for api
+ *  returns all of beekeeper basic information  (beekeeper names, beekeeper location)
+ */
 pub async fn bk_index(httpReq: HttpRequest) -> impl Responder {
     let request = BeekeeperRequestModule{req: httpReq};
     let _ = request.to_request();
@@ -13,6 +16,9 @@ pub async fn bk_index(httpReq: HttpRequest) -> impl Responder {
 }
 
 
+// temporally shortcut of request, response adaptor
+
+// map request to domain model
 pub struct BeekeeperRequestModule{
     req: HttpRequest
 }
@@ -21,6 +27,7 @@ impl BeekeeperRequestModule{
     pub fn to_request(&self) {}
 }
 
+// unmap from logic domain model to response type
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BeekeeperResponseModule{
     pub beekeepers: Vec<Beekeeper>
