d0e7107ace1f785afd5b63ff7065a30cb55a41c7
daiki <daiki@local>
2021-10-17 23:31:19 +0900

    support CORS

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::http::header;
 
 use hms_project::controllers::pages::index::index;
 use hms_project::controllers::apis::beekeepers::bk_index;
@@ -7,13 +9,27 @@ use hms_project::controllers::apis::prefectures::get_all_prefectures;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-     HttpServer::new(|| App::new()
+     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin_fn(|origin, _req_head| {
+                true
+            })
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .supports_credentials()
+            .max_age(3600);  
+        
+        App::new()
+        .wrap(cors)
          .route("/", web::get().to(index))
          .route("/api/v1/beekeepers", web::get().to(bk_index))
          .route("/api/v1/honeies", web::get().to(honeies_index))
          .route("/api/v1/prefectures", web::get().to(get_all_prefectures))
-     )
+      } 
+    )
         .bind("127.0.0.1:8080")?
         .run()
         .await
+    
 }
